9bac8ef2d1a95759cd0ed1b5583d153a47543bda
Lusca Lima <lusca.lima@outlook.com>
2022-04-25 22:59:29 -0300

    feat: implements the project entity

--- src/project.js
+++ src/project.js
@@ -1,1 +1,29 @@
-// TODO: Dê uma olhada no projeto oficial do módulo 06 (Expressões Regulares - RegExp) para implementar este arquivo.
+const { evaluateRegex } = require("./util");
+
+class Project {
+  constructor({ título, link, autor, indexadoresnorma }) {
+    const splitInfoRegex = evaluateRegex(/,/g);
+    const idRegex = evaluateRegex(/(\d+)$/g);
+    const numberYearRegex = evaluateRegex(/(\d+)/g);
+    const firstLastNamesRegex = evaluateRegex(/\s/g);
+    const [number, year] = título.match(numberYearRegex);
+    const authors = autor.split(splitInfoRegex).map((author) => {
+      const names = author.trim().split(firstLastNamesRegex);
+      const first = names.shift();
+      const last = names.pop();
+      return { nome: [first, last].join(" ") };
+    });
+    const indexers = indexadoresnorma
+      .split(splitInfoRegex)
+      .map((indexer) => indexer.trim());
+
+    this.id = link.match(idRegex).join();
+    this.url = link;
+    this.numero = number;
+    this.ano = year;
+    this.autores = authors;
+    this.indexadores = indexers;
+  }
+}
+
+module.exports = Project;
--- test/project.test.js
+++ test/project.test.js
@@ -1,1 +1,43 @@
-// TODO: Dê uma olhada no projeto oficial do módulo 06 (Expressões Regulares - RegExp) para implementar este arquivo.
+const { describe, it } = require("mocha");
+const { expect } = require("chai");
+const Project = require("../src/project");
+
+describe("Project", () => {
+  it("should generate a project instance from properties list", () => {
+    const content = {
+      título: "Projeto de lei 580/2016",
+      link: "http://www.al.sp.gov.br/propositura?id=1323286",
+      autor: "Marcia Lia, Marcia Sol Lua",
+      etapa: "PAUTA",
+      ementa:
+        "Estabelece normas gerais para a realização de Concurso Público pela Administração Pública Direta e Indireta do Estado.",
+      indexadoresnorma:
+        "NORMAS, REALIZAÇÃO, CONCURSO PÚBLICO ESTADUAL, ESTADO DE SÃO PAULO, ADMINISTRAÇÃO PÚBLICA DIRETA E INDIRETA",
+    };
+
+    const result = new Project(content);
+    const expected = {
+      id: "1323286",
+      numero: "580",
+      ano: "2016",
+      autores: [
+        {
+          nome: "Marcia Lia",
+        },
+        {
+          nome: "Marcia Lua",
+        },
+      ],
+      url: "http://www.al.sp.gov.br/propositura?id=1323286",
+      indexadores: [
+        "NORMAS",
+        "REALIZAÇÃO",
+        "CONCURSO PÚBLICO ESTADUAL",
+        "ESTADO DE SÃO PAULO",
+        "ADMINISTRAÇÃO PÚBLICA DIRETA E INDIRETA",
+      ],
+    };
+
+    expect(result).to.be.deep.equal(expected);
+  });
+});
