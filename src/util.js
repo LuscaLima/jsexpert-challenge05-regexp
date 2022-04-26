d8141fc9e8ec32cbed6f6abdf4e29d16876db33b
Lusca Lima <lusca.lima@outlook.com>
2022-04-25 22:58:01 -0300

    feat: implements the utils

--- src/util.js
+++ src/util.js
@@ -1,1 +1,18 @@
-// TODO: Dê uma olhada no projeto oficial do módulo 06 (Expressões Regulares - RegExp) para implementar este arquivo.
+const safeRegex = require("safe-regex");
+
+class InvalidRegexError extends Error {
+  constructor(expression) {
+    super(`The ${expression} is unsafe`);
+    this.name = "InvalidRegexError";
+  }
+}
+
+function evaluateRegex(regex) {
+  const isSafe = safeRegex(regex);
+
+  if (isSafe) return regex;
+
+  throw new InvalidRegexError(regex);
+}
+
+module.exports = { evaluateRegex, InvalidRegexError };
--- test/util.test.js
+++ test/util.test.js
@@ -1,1 +1,21 @@
-// TODO: Dê uma olhada no projeto oficial do módulo 06 (Expressões Regulares - RegExp) para implementar este arquivo.
+const { describe, it } = require("mocha");
+const { expect } = require("chai");
+const { evaluateRegex, InvalidRegexError } = require("../src/util");
+
+describe("Util", () => {
+  it("#evalutateRegex should throw an error using an unsafe regex", () => {
+    const unsafeRegex = /^([a-zA-Z0-9]+\s?)+$/;
+
+    expect(() => evaluateRegex(unsafeRegex)).to.throw(
+      InvalidRegexError,
+      `The ${unsafeRegex} is unsafe`
+    );
+  });
+
+  it("#evalutateRegex should not throw an error using a safe regex", () => {
+    const safeRegex = /^([a-z])$/;
+
+    expect(() => evaluateRegex(safeRegex)).to.not.throw();
+    expect(evaluateRegex(safeRegex)).to.be.deep.equal(safeRegex);
+  });
+});
