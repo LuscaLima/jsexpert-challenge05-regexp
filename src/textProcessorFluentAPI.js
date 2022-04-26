d9a24a8a5fbb53107d61918506af97136ed3e7fc
Lusca Lima <lusca.lima@outlook.com>
2022-04-25 22:59:39 -0300

    feat: implements the text processor

--- src/textProcessorFluentAPI.js
+++ src/textProcessorFluentAPI.js
@@ -1,1 +1,61 @@
-// TODO: Dê uma olhada no projeto oficial do módulo 06 (Expressões Regulares - RegExp) para implementar este arquivo.
+const { evaluateRegex } = require("./util");
+const Project = require("./project");
+
+class TextProcessorFluentAPI {
+  #content;
+
+  constructor(content) {
+    this.#content = content;
+  }
+
+  extractProjectData() {
+    const dataRegex = evaluateRegex(/^(.*);$/gm);
+    const project = this.#content.match(dataRegex);
+    this.#content = project;
+    return this;
+  }
+
+  extractColumns() {
+    const columnsRow = this.#content.shift();
+    const columnsSplitRegex = evaluateRegex(/;/g);
+    const columns = columnsRow.split(columnsSplitRegex).filter(Boolean);
+    this.#content = {
+      columns,
+      lines: this.#content,
+    };
+    return this;
+  }
+
+  extractLines() {
+    const lineSplitRegex = evaluateRegex(/;/g);
+    this.#content.lines = this.#content.lines.map((line) =>
+      line.split(lineSplitRegex).filter(Boolean)
+    );
+    return this;
+  }
+
+  mountData() {
+    this.#content = this.#content.lines.map((line) =>
+      this.#content.columns.reduce(
+        (data, column, index) => ({
+          ...data,
+          [column]: line[index] || "",
+        }),
+        {}
+      )
+    );
+    return this;
+  }
+
+  mapProject() {
+    this.#content = this.#content.map((project) => new Project(project));
+
+    return this;
+  }
+
+  build() {
+    return this.#content;
+  }
+}
+
+module.exports = TextProcessorFluentAPI;
--- test/textProcessorFluentAPI.test.js
+++ test/textProcessorFluentAPI.test.js
@@ -1,1 +1,187 @@
-// TODO: Dê uma olhada no projeto oficial do módulo 06 (Expressões Regulares - RegExp) para implementar este arquivo.
+const { describe, it } = require("mocha");
+const { expect } = require("chai");
+
+const TextProcessorFluentAPI = require("../src/textProcessorFluentAPI");
+const mock = require("./mock/valid");
+
+describe("TextProcessorFluentAPI", () => {
+  it("#build", () => {
+    const result = new TextProcessorFluentAPI(mock).build();
+
+    expect(result).to.be.deep.equal(mock);
+  });
+
+  it("#extractData", () => {
+    const result = new TextProcessorFluentAPI(mock)
+      .extractProjectData()
+      .build();
+    const expected = [
+      "título;link;autor;etapa;ementa;indexadoresnorma;",
+      "Projeto de lei 584/2016;http://www.al.sp.gov.br/propositura?id=1322563;Jorge Wilson Xerife do Consumidor;PAUTA;Dispõe sobre a inclusão de cláusula nos contratos de adesão aos serviços de telefonia fixa, de telefonia móvel e de banda larga móvel, e dá outras providências.;CONTRATO, OBRIGATORIEDADE, CLÁUSULA, SERVIÇO, TELEFONIA MÓVEL, TELEFONIA FIXA, PRAZO, INCLUSÃO, RESCISÃO CONTRATUAL, LIBERAÇÃO;",
+      "Projeto de lei 580/2016;http://www.al.sp.gov.br/propositura?id=1323286;Marcia Lia;PAUTA;Estabelece normas gerais para a realização de Concurso Público pela Administração Pública Direta e Indireta do Estado.;NORMAS, REALIZAÇÃO, CONCURSO PÚBLICO ESTADUAL, ESTADO DE SÃO PAULO, ADMINISTRAÇÃO PÚBLICA DIRETA E INDIRETA;",
+      "Projeto de lei 545/2016;http://www.al.sp.gov.br/propositura?id=1322832;Roberto Morais, Itamar Borges;PAUTA;Altera a Lei nº 13.550, de 2009, que dispõe sobre a utilização e proteção da vegetação nativa do Bioma Cerrado no Estado de São Paulo.;",
+    ];
+
+    expect(result).to.be.deep.equal(expected);
+  });
+
+  it("#extractColumns", () => {
+    const content = [
+      "título;link;autor;etapa;ementa;indexadoresnorma;",
+      "Projeto de lei 545/2016;http://www.al.sp.gov.br/propositura?id=1322832;Roberto Morais, Itamar Borges;PAUTA;Altera a Lei nº 13.550, de 2009, que dispõe sobre a utilização e proteção da vegetação nativa do Bioma Cerrado no Estado de São Paulo.;",
+    ];
+    const expected = {
+      columns: [
+        "título",
+        "link",
+        "autor",
+        "etapa",
+        "ementa",
+        "indexadoresnorma",
+      ],
+      lines: [
+        "Projeto de lei 545/2016;http://www.al.sp.gov.br/propositura?id=1322832;Roberto Morais, Itamar Borges;PAUTA;Altera a Lei nº 13.550, de 2009, que dispõe sobre a utilização e proteção da vegetação nativa do Bioma Cerrado no Estado de São Paulo.;",
+      ],
+    };
+    const result = new TextProcessorFluentAPI(content).extractColumns().build();
+
+    expect(result).to.be.deep.equal(expected);
+  });
+
+  it("#extractLines", () => {
+    const content = {
+      columns: [
+        "título",
+        "link",
+        "autor",
+        "etapa",
+        "ementa",
+        "indexadoresnorma",
+      ],
+      lines: [
+        "Projeto de lei 545/2016;http://www.al.sp.gov.br/propositura?id=1322832;Roberto Morais, Itamar Borges;PAUTA;Altera a Lei nº 13.550, de 2009, que dispõe sobre a utilização e proteção da vegetação nativa do Bioma Cerrado no Estado de São Paulo.;",
+      ],
+    };
+    const expected = {
+      columns: [
+        "título",
+        "link",
+        "autor",
+        "etapa",
+        "ementa",
+        "indexadoresnorma",
+      ],
+      lines: [
+        [
+          "Projeto de lei 545/2016",
+          "http://www.al.sp.gov.br/propositura?id=1322832",
+          "Roberto Morais, Itamar Borges",
+          "PAUTA",
+          "Altera a Lei nº 13.550, de 2009, que dispõe sobre a utilização e proteção da vegetação nativa do Bioma Cerrado no Estado de São Paulo.",
+        ],
+      ],
+    };
+    const result = new TextProcessorFluentAPI(content).extractLines().build();
+
+    expect(result).to.be.deep.equal(expected);
+  });
+
+  it("#mountData", () => {
+    const content = {
+      columns: [
+        "título",
+        "link",
+        "autor",
+        "etapa",
+        "ementa",
+        "indexadoresnorma",
+      ],
+      lines: [
+        [
+          "Projeto de lei 580/2016",
+          "http://www.al.sp.gov.br/propositura?id=1323286",
+          "Marcia Lia",
+          "PAUTA",
+          "Estabelece normas gerais para a realização de Concurso Público pela Administração Pública Direta e Indireta do Estado.",
+          "NORMAS, REALIZAÇÃO, CONCURSO PÚBLICO ESTADUAL, ESTADO DE SÃO PAULO, ADMINISTRAÇÃO PÚBLICA DIRETA E INDIRETA",
+        ],
+        [
+          "Projeto de lei 545/2016",
+          "http://www.al.sp.gov.br/propositura?id=1322832",
+          "Roberto Morais, Itamar Borges",
+          "PAUTA",
+          "Altera a Lei nº 13.550, de 2009, que dispõe sobre a utilização e proteção da vegetação nativa do Bioma Cerrado no Estado de São Paulo.",
+        ],
+      ],
+    };
+
+    const expected = [
+      {
+        título: "Projeto de lei 580/2016",
+        link: "http://www.al.sp.gov.br/propositura?id=1323286",
+        autor: "Marcia Lia",
+        etapa: "PAUTA",
+        ementa:
+          "Estabelece normas gerais para a realização de Concurso Público pela Administração Pública Direta e Indireta do Estado.",
+        indexadoresnorma:
+          "NORMAS, REALIZAÇÃO, CONCURSO PÚBLICO ESTADUAL, ESTADO DE SÃO PAULO, ADMINISTRAÇÃO PÚBLICA DIRETA E INDIRETA",
+      },
+      {
+        título: "Projeto de lei 545/2016",
+        link: "http://www.al.sp.gov.br/propositura?id=1322832",
+        autor: "Roberto Morais, Itamar Borges",
+        etapa: "PAUTA",
+        ementa:
+          "Altera a Lei nº 13.550, de 2009, que dispõe sobre a utilização e proteção da vegetação nativa do Bioma Cerrado no Estado de São Paulo.",
+        indexadoresnorma: "",
+      },
+    ];
+
+    const result = new TextProcessorFluentAPI(content).mountData().build();
+
+    expect(result).to.be.deep.equal(expected);
+  });
+
+  it("#mapProject", () => {
+    const content = [
+      {
+        título: "Projeto de lei 580/2016",
+        link: "http://www.al.sp.gov.br/propositura?id=1323286",
+        autor: "Marcia Lia, Marcia Sol Lua",
+        etapa: "PAUTA",
+        ementa:
+          "Estabelece normas gerais para a realização de Concurso Público pela Administração Pública Direta e Indireta do Estado.",
+        indexadoresnorma:
+          "NORMAS, REALIZAÇÃO, CONCURSO PÚBLICO ESTADUAL, ESTADO DE SÃO PAULO, ADMINISTRAÇÃO PÚBLICA DIRETA E INDIRETA",
+      },
+    ];
+
+    const expected = [
+      {
+        id: "1323286",
+        numero: "580",
+        ano: "2016",
+        autores: [
+          {
+            nome: "Marcia Lia",
+          },
+          {
+            nome: "Marcia Lua",
+          },
+        ],
+        url: "http://www.al.sp.gov.br/propositura?id=1323286",
+        indexadores: [
+          "NORMAS",
+          "REALIZAÇÃO",
+          "CONCURSO PÚBLICO ESTADUAL",
+          "ESTADO DE SÃO PAULO",
+          "ADMINISTRAÇÃO PÚBLICA DIRETA E INDIRETA",
+        ],
+      },
+    ];
+
+    const result = new TextProcessorFluentAPI(content).mapProject().build();
+
+    expect(result).to.be.deep.equal(expected);
+  });
+});
